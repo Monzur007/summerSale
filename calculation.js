0dbb4cd3ef35dc3caa840d02a58c2ed833660af2
Md Monzur Hossain <MHossain@rossem.local>
2023-10-05 17:47:24 +0600

    added

---
+++ calculation.js
@@ -0,0 +1,29 @@
+
+
+    function myFunction() {
+          document.getElementById("pl1").innerHTML = "Sports Cap";
+          document.getElementById("tp").innerHTML = "49.00";
+}
+function myFunction2() {
+    document.getElementById("pl2").innerHTML = "Jersey";
+    document.getElementById("tp").innerHTML = "69.00";
+}
+function myFunction3() {
+    document.getElementById("pl3").innerHTML = "cates";
+    document.getElementById("tp").innerHTML = "159.00";
+}
+function coupon (){
+    const couponField = document.getElementById('input7');
+    const couponValueText = couponField.value ;
+    const value = parseFloat(couponValueText);
+    console.log (value);
+}
+
+
+const totalPrice = value;
+
+const totalPrice1 = document.getElementById ('tp');
+tp.innerText = value;
+
+
+
