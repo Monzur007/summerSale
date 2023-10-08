17ad575bdd9bf4ac0c8b06a6974b56fbc61c50ed
Md Monzur Hossain <MHossain@rossem.local>
2023-10-08 16:54:26 +0600

    added

--- calculation.js
+++ calculation.js
@@ -2,28 +2,111 @@
 
     function myFunction() {
           document.getElementById("pl1").innerHTML = "Sports Cap";
-          document.getElementById("tp").innerHTML = "49.00";
+       
+        const priceField = document.getElementById('taka');
+        const priceAmountString = priceField.innerText;
+       
+        const newPriceAmount = parseFloat(priceAmountString);
+
+        const priceTotalElement = document.getElementById('tp');
+        const previousPriceTotalString = priceTotalElement.innerText;
+        const previousPriceTotal= parseFloat(previousPriceTotalString);
+
+        const currentPriceTotal = previousPriceTotal + newPriceAmount;
+        priceTotalElement.innerText = currentPriceTotal; 
+
 }
+
+
+
+
+
+
 function myFunction2() {
     document.getElementById("pl2").innerHTML = "Jersey";
-    document.getElementById("tp").innerHTML = "69.00";
+    
+    const priceField = document.getElementById('taka2');
+        const priceAmountString = priceField.innerText;
+       
+        const newPriceAmount = parseFloat(priceAmountString);
+
+        const priceTotalElement = document.getElementById('tp');
+        const previousPriceTotalString = priceTotalElement.innerText;
+        const previousPriceTotal= parseFloat(previousPriceTotalString);
+
+        const currentPriceTotal = previousPriceTotal + newPriceAmount;
+        priceTotalElement.innerText = currentPriceTotal; 
+
 }
 function myFunction3() {
     document.getElementById("pl3").innerHTML = "cates";
-    document.getElementById("tp").innerHTML = "159.00";
+    const priceField = document.getElementById('taka3');
+    const priceAmountString = priceField.innerText;
+   
+    const newPriceAmount = parseFloat(priceAmountString);
+
+    const priceTotalElement = document.getElementById('tp');
+    const previousPriceTotalString = priceTotalElement.innerText;
+    const previousPriceTotal= parseFloat(previousPriceTotalString);
+
+    const currentPriceTotal = previousPriceTotal + newPriceAmount;
+    priceTotalElement.innerText = currentPriceTotal; 
 }
-function coupon (){
-    const couponField = document.getElementById('input7');
-    const couponValueText = couponField.value ;
-    const value = parseFloat(couponValueText);
-    console.log (value);
+
+function myFunction4() {
+    document.getElementById("pl4").innerHTML = "Chair";
+    const priceField = document.getElementById('taka4');
+    const priceAmountString = priceField.innerText;
+   
+    const newPriceAmount = parseFloat(priceAmountString);
+
+    const priceTotalElement = document.getElementById('tp');
+    const previousPriceTotalString = priceTotalElement.innerText;
+    const previousPriceTotal= parseFloat(previousPriceTotalString);
+
+    const currentPriceTotal = previousPriceTotal + newPriceAmount;
+    priceTotalElement.innerText = currentPriceTotal; 
+}
+
+function myFunction5() {
+    document.getElementById("pl5").innerHTML = "Child Play Chair";
+    const priceField = document.getElementById('taka5');
+    const priceAmountString = priceField.innerText;
+   
+    const newPriceAmount = parseFloat(priceAmountString);
+
+    const priceTotalElement = document.getElementById('tp');
+    const previousPriceTotalString = priceTotalElement.innerText;
+    const previousPriceTotal= parseFloat(previousPriceTotalString);
+
+    const currentPriceTotal = previousPriceTotal + newPriceAmount;
+    priceTotalElement.innerText = currentPriceTotal; 
+}
+function myFunction6() {
+    document.getElementById("pl6").innerHTML = "Sofa";
+    const priceField = document.getElementById('taka6');
+    const priceAmountString = priceField.innerText;
+   
+    const newPriceAmount = parseFloat(priceAmountString);
+
+    const priceTotalElement = document.getElementById('tp');
+    const previousPriceTotalString = priceTotalElement.innerText;
+    const previousPriceTotal= parseFloat(previousPriceTotalString);
+
+    const currentPriceTotal = previousPriceTotal + newPriceAmount;
+    priceTotalElement.innerText = currentPriceTotal; 
+
+    // const discount = 0.2 * currentPriceTotal
 }
 
 
-const totalPrice = value;
 
-const totalPrice1 = document.getElementById ('tp');
-tp.innerText = value;
+
+
+
+
+
+
 
 
 
